aa02f104d8f21f21890ee4aa77f9ff0f33de0277
anlehoang6789 <anlehoang6789@gmail.com>
2024-04-30 15:05:49 +0700

    ui: config ui layout for all routes

---
+++ src/layouts/admin_layout/HeaderAdmin/HeaderAdmin.tsx
@@ -0,0 +1,39 @@
+import { LogoutOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar, Dropdown, MenuProps, Modal, Spin } from 'antd';
+import { Header } from 'antd/es/layout/layout';
+
+export default function HeaderAdmin() {
+  const items: MenuProps['items'] = [
+    {
+      key: '1',
+      icon: <UserOutlined></UserOutlined>,
+      label: 'Profile'
+    },
+    {
+      key: '2',
+      icon: <LogoutOutlined></LogoutOutlined>,
+      label: 'Logout'
+    }
+  ];
+
+  return (
+    <Header className='fixed z-50 flex w-full justify-between border-b border-gray-200 bg-white px-5'>
+      <Dropdown menu={{ items }} placement='bottomRight' trigger={['click']} arrow>
+        <Avatar
+          className='fixed right-4 top-3 cursor-pointer'
+          size={'large'}
+          icon={<UserOutlined />}
+          src={
+            'https://firebasestorage.googleapis.com/v0/b/voguary.appspot.com/o/Avatar%2Favatar_1.jpg?alt=media&token=c9cc1417-7534-4a4b-b8ff-1e018088cea7'
+          }
+        />
+      </Dropdown>
+      <Modal footer={null} closable={false}>
+        <div className='flex flex-col items-center justify-center'>
+          <Spin size='large'></Spin>
+          <span>Đang đăng xuất...</span>
+        </div>
+      </Modal>
+    </Header>
+  );
+}
---
+++ src/layouts/admin_layout/SiderAdmin/Sider.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import { Menu, MenuProps } from 'antd';
+import Sider from 'antd/es/layout/Sider';
+import { useNavigate } from 'react-router-dom';
+import { cn } from 'utils/cn';
+import { MdDashboard, MdManageAccounts, MdInventory, MdOutlineMenu } from 'react-icons/md';
+import { FaUsersViewfinder } from 'react-icons/fa6';
+import { IoMdPersonAdd } from 'react-icons/io';
+
+export default function MySider() {
+  type MenuItem = Required<MenuProps>['items'][number];
+
+  function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode, children?: MenuItem[]): MenuItem {
+    return {
+      key,
+      icon,
+      children,
+      label
+    } as MenuItem;
+  }
+
+  const navigate = useNavigate();
+  const [collapsed, setCollapsed] = useState(window.innerWidth < 1280);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setCollapsed(window.innerWidth < 1280);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const getConditionalItems = (): MenuItem[] => {
+    return [
+      getItem('Dashboard', 'dashboard', <MdDashboard />),
+      getItem('View all inventory', 'inventory', <MdInventory />),
+      getItem('Manage user', 'sub2', <MdManageAccounts />, [
+        getItem('View all user', 'accountAll', <FaUsersViewfinder />),
+        getItem('Create account for staff', 'createAccount', <IoMdPersonAdd />)
+      ])
+    ];
+  };
+  const navUrl = new Map<string, string>();
+  navUrl.set('dashboard', '/admin/');
+
+  return (
+    <>
+      <Sider
+        theme='light'
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+        className='overflow-hidden border-r-[1px]'
+        trigger={
+          <div className='w-full border-r-[1px] border-t-[1px] flex items-center justify-center text-2xl pt-2'>
+            <MdOutlineMenu />
+          </div>
+        }
+        width={256}
+      >
+        <div className='border-r-[1px] border-gray-200'>
+          <img
+            src='https://firebasestorage.googleapis.com/v0/b/voguary.appspot.com/o/Logo_Website%2Flogo_white_bg.jpg?alt=media&token=d4b8a3be-870c-469c-b93f-a7c8cc6a6bbe'
+            alt='logo'
+            className={cn('mx-auto max-w-[100px] max-h-[100px]', {
+              hidden: collapsed
+            })}
+          />
+          <img
+            src='https://firebasestorage.googleapis.com/v0/b/voguary.appspot.com/o/Logo_Website%2Flogo_white_bg.jpg?alt=media&token=d4b8a3be-870c-469c-b93f-a7c8cc6a6bbe'
+            alt='logo'
+            className={cn('mx-auto max-w-[50px]  py-2', {
+              hidden: !collapsed
+            })}
+          />
+        </div>
+        <Menu
+          defaultSelectedKeys={['1']}
+          mode='inline'
+          items={getConditionalItems()}
+          onSelect={(e) => {
+            const link = navUrl.get(e.key);
+            if (link) {
+              navigate(link);
+            }
+          }}
+        ></Menu>
+      </Sider>
+    </>
+  );
+}

1faa3f3c79ef634d4bbd10ed1118752e0fe33f4c
giaminh05 <hagiaminh29062002@gmail.com>
2024-05-05 15:32:13 +0700

    UI: fix profile client

--- src/layouts/client_layout/HeaderClient/Header.tsx
+++ src/layouts/client_layout/HeaderClient/Header.tsx
@@ -1,5 +1,5 @@
 import { ShoppingCartOutlined } from '@ant-design/icons';
-import { Button, Menu } from 'antd';
+import { Button, ConfigProvider, Menu } from 'antd';
 import Search from 'antd/es/input/Search';
 import MenuItem from 'antd/es/menu/MenuItem';
 import '../../../App.css';
@@ -9,7 +9,12 @@ export default function Header() {
   return (
     <div className='container-fluid'>
       <div className='header bg-white shadow-md'>
-        <Menu mode='horizontal' className='menu flex items-center'>
+        <Menu
+          mode='horizontal'
+          className='menu flex items-center '
+          // triggerSubMenuAction='click'
+          // style={{ borderBottom: 'none', lineHeight: '0px' }}
+        >
           <Link to={'/'}>
             <img
               src='https://firebasestorage.googleapis.com/v0/b/voguary.appspot.com/o/Logo_Website%2Flogo_white_bg.jpg?alt=media&token=d4b8a3be-870c-469c-b93f-a7c8cc6a6bbe'
@@ -18,7 +23,7 @@ export default function Header() {
             ></img>
           </Link>
 
-          <MenuItem className='text-xl'>Thể loại</MenuItem>
+          <MenuItem className='text-xl '>Thể loại</MenuItem>
 
           <MenuItem>
             <Search size='large' placeholder='Tìm kiếm nội dung bất kỳ' style={{ width: 660, paddingTop: '6px' }} />

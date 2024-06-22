1398c5c2249ae02e496176a524b5948c8224a7f8
Nguyen Hong Ngoc <134280054+pby1905@users.noreply.github.com>
2024-06-22 23:30:57 +0700

    Add Image Homepage

--- src/components/CardHomepage/Carousel.tsx
+++ src/components/CardHomepage/Carousel.tsx
@@ -1,22 +1,35 @@
 import Slider from 'react-slick';
 
-const firebaseImageUrl1 =
-  'https://firebasestorage.googleapis.com/v0/b/voguary.appspot.com/o/Placeholder%2F1600x400.png?alt=media&token=f7acacf5-8923-44c1-8c58-8c376be0ace7';
-
 export default function CustomCarousel() {
   return (
     <Slider autoplay dots>
       <div>
-        <img src={firebaseImageUrl1} alt='Slide 1' className='w-full h-48 md:h-64 lg:h-72 xl:h-80' />
+        <img
+          src='https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/448749977_122114863820327879_5976207896387936120_n.png?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGj8enHAfechtjXs5IPBsjnlhIXoD8IFXOWEhegPwgVc4H6Y5DS5EFCaCtSAVQSGDJkB0vB6Wv7icDe78yidPpS&_nc_ohc=C6KxC6ZmwZAQ7kNvgHNmz69&_nc_ht=scontent.fsgn2-9.fna&oh=00_AYAkDLQyMp6H2RLWNXhMqL2Pr_sxps1XkHkMV4Czy5jQHg&oe=667C9AC6'
+          alt='Slide 1'
+          className='w-full h-full md:h-64 lg:h-72 xl:h-80'
+        />
       </div>
       <div>
-        <img src={firebaseImageUrl1} alt='Slide 2' className='w-full h-48 md:h-64 lg:h-72 xl:h-80' />
+        <img
+          src='https://scontent.fsgn2-9.fna.fbcdn.net/v/t39.30808-6/448653275_122114522996327879_3144253116482339838_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFHHVuv6ihgsBQyG_xaqFSAiyT004xTyqGLJPTTjFPKoWbmdUDZTSddc7jkfkNSrG9k__q0pZFpQ--ffErWTPit&_nc_ohc=NgF_NiOq3pQQ7kNvgFko4PW&_nc_ht=scontent.fsgn2-9.fna&oh=00_AYD1ZVCPl4_668o-e2HOM67NOj2Ti8leOwms73CIunWCtQ&oe=667C91F5'
+          alt='Slide 2'
+          className='w-full h-full md:h-64 lg:h-72 xl:h-80'
+        />
       </div>
       <div>
-        <img src={firebaseImageUrl1} alt='Slide 3' className='w-full h-48 md:h-64 lg:h-72 xl:h-80' />
+        <img
+          src='https://scontent.fsgn2-7.fna.fbcdn.net/v/t39.30808-6/447855712_122110828136327879_1479968347455933902_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE9LEslRYR5Rk3DrOjmT9BMiKegcLq1m_WIp6BwurWb9dY3ifp3axTXsi3Zo7wKqeutDcIpS1XU1hVTeUsrVBwC&_nc_ohc=Sgg2UyN8_aIQ7kNvgHRNtjD&_nc_ht=scontent.fsgn2-7.fna&oh=00_AYAIDzRMqnlYFY-zMC5_IlkAucwhkn9bLy4i9NmU7erQ2Q&oe=667C7EDD'
+          alt='Slide 3'
+          className='w-full h-full md:h-64 lg:h-72 xl:h-80'
+        />
       </div>
       <div>
-        <img src={firebaseImageUrl1} alt='Slide 4' className='w-full h-48 md:h-64 lg:h-72 xl:h-80' />
+        <img
+          src='https://scontent.fsgn2-5.fna.fbcdn.net/v/t39.30808-6/448812515_122115251762327879_3606431316895751068_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFHYjhrFlAWkNKATnL9mfdla2AWibNw1_NrYBaJs3DX822o79nlYUks0dyw3D4UX9xPwOUYnlhWVJL4gOFkTYTY&_nc_ohc=9M2MsXKx-s4Q7kNvgHdb922&_nc_ht=scontent.fsgn2-5.fna&oh=00_AYC2y5OtOKYphO7NV7wyB1JpkoTE0gHSoDT1zuqKpAhSPw&oe=667C9B8A'
+          alt='Slide 4'
+          className='w-full h-full md:h-64 lg:h-72 xl:h-80'
+        />
       </div>
     </Slider>
   );
--- src/components/CardHomepage/Gallery.tsx
+++ src/components/CardHomepage/Gallery.tsx
@@ -8,45 +8,22 @@ export default function Gallery() {
         <div className='grid gap-4'>
           <div>
             <img
-              className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg'
+              className='h-90 max-w-full rounded-lg'
+              src='https://i-kinhdoanh.vnecdn.net/2019/11/06/mau-500-8539-1573015927.jpg'
               alt=''
             />
           </div>
           <div>
             <img
               className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg'
+              src='https://i.pinimg.com/736x/b9/7a/26/b97a26f802ce06046f5a47aacd5a0d10.jpg'
               alt=''
             />
           </div>
           <div>
             <img
               className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg'
-              alt=''
-            />
-          </div>
-        </div>
-        <div className='grid gap-4'>
-          <div>
-            <img
-              className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg'
-              alt=''
-            />
-          </div>
-          <div>
-            <img
-              className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg'
-              alt=''
-            />
-          </div>
-          <div>
-            <img
-              className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg'
+              src='https://i.pinimg.com/736x/61/cf/b5/61cfb55382ad98d56c98b5392a619cf5.jpg'
               alt=''
             />
           </div>
@@ -55,21 +32,21 @@ export default function Gallery() {
           <div>
             <img
               className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg'
+              src='https://i.pinimg.com/736x/d3/ca/71/d3ca71311d3fbc916a5ca5f9108cfe6b.jpg'
               alt=''
             />
           </div>
           <div>
             <img
               className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg'
+              src='https://i.pinimg.com/1200x/ac/65/cd/ac65cd9b2141c29e75194267852c74be.jpg'
               alt=''
             />
           </div>
           <div>
             <img
               className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg'
+              src='https://lavenderstudio.com.vn/wp-content/uploads/2017/03/chup-san-pham-quan-ao-dep.jpg'
               alt=''
             />
           </div>
@@ -78,21 +55,44 @@ export default function Gallery() {
           <div>
             <img
               className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg'
+              src='https://tronhouse.com/assets/data/editor/source/3-meo-chup-lookbook-thoi-trang-dep-nhu-mo-khien-ai-cung-phai-nguoc-nhin/chup-lookbook-4.jpg'
               alt=''
             />
           </div>
           <div>
             <img
               className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg'
+              src='https://media.istockphoto.com/id/1367180724/vi/anh/ph%E1%BB%A5-n%E1%BB%AF-tr%E1%BA%BB-s%C3%A0nh-%C4%91i%E1%BB%87u-trong-trang-ph%E1%BB%A5c-pastel-%C4%91%E1%BB%A9ng-c%C3%B9ng-nhau-kh%C3%A1i-ni%E1%BB%87m-th%E1%BB%9Di-trang-%E1%BA%A3nh-ch%E1%BB%A9ng-kho%C3%A1n.jpg?s=612x612&w=0&k=20&c=JCLitzmS8rEUXV6rF77y4A_nEcVCKmJJVyQB164pACY='
               alt=''
             />
           </div>
           <div>
             <img
               className='h-auto max-w-full rounded-lg'
-              src='https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg'
+              src='https://image.donghohaitrieu.com/wp-content/uploads/2023/09/11-10-thuong-hieu-thoi-trang-nu-noi-tieng-nhat-gia-binh-dan.jpg'
+              alt=''
+            />
+          </div>
+        </div>
+        <div className='grid gap-4'>
+          <div>
+            <img
+              className='h-auto max-w-full rounded-lg'
+              src='https://i.pinimg.com/originals/0c/ab/db/0cabdb8e545e8b6b9cfb1a81ebf8bf15.png'
+              alt=''
+            />
+          </div>
+          <div>
+            <img
+              className='h-auto max-w-full rounded-lg'
+              src='https://i.pinimg.com/736x/81/df/7e/81df7e233790388b2587c329a65aa199.jpg'
+              alt=''
+            />
+          </div>
+          <div>
+            <img
+              className='h-auto max-w-full rounded-lg'
+              src='https://camfashion.vn/wp-content/uploads/2023/07/Set-Ao-Tweed-Jacket-Chan-Vay-A008.webp'
               alt=''
             />
           </div>

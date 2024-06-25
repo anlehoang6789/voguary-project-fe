f237bfb501966861f4a505e4e8836518bca9d93f
anlehoang6789 <anlehoang6789@gmail.com>
2024-06-25 16:03:40 +0700

    feat: add product for staff

--- src/services/product.services.ts
+++ src/services/product.services.ts
@@ -109,5 +109,7 @@ export const {
   useGetFeedbackQuery,
   useGetProductRecommendationsQuery,
   useGetProductDetailsInforQuery,
-  useGetFilterProductsQuery
+  useGetFilterProductsQuery,
+  useAddProductMutation,
+  useAddProductDetailMutation
 } = productApi;

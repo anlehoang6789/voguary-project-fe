dcd66a46752d0daf3aca03b2654a0bb0da6e6527
anlehoang6789 <anlehoang6789@gmail.com>
2024-07-04 13:43:45 +0700

    feat: call dashboard 4 card

---
+++ src/services/dashboard.services.ts
@@ -0,0 +1,67 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { UserLoginResponse } from 'types/Account.type';
+import {
+  GetTotalItemsInStockResponse,
+  GetTotalReturnedOrdersResponse,
+  GetTotalRevenueResponse,
+  GetTotalUsersCustomerResponse,
+  GetTotalUsersStaffResponse
+} from 'types/Dashboard.type';
+import baseUrl from 'utils/http';
+
+export const dashboardApi = createApi({
+  reducerPath: 'dashboardApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+    prepareHeaders: (headers) => {
+      const user = localStorage.getItem('userLogin');
+      if (user) {
+        const userData = JSON.parse(user) as UserLoginResponse;
+        const token = userData ? userData.token : '';
+        headers.set('authorization', `Bearer ${token}`);
+      }
+      return headers;
+    }
+  }),
+  refetchOnMountOrArgChange: true,
+  endpoints: (build) => ({
+    getTotalRevenue: build.query<GetTotalRevenueResponse, void>({
+      query: () => ({
+        url: `Dashboard/TotalRevenue`,
+        method: 'GET'
+      })
+    }),
+    getTotalUsersCustomer: build.query<GetTotalUsersCustomerResponse, void>({
+      query: () => ({
+        url: `Dashboard/TotalUsersCustomer`,
+        method: 'GET'
+      })
+    }),
+    getTotalUsersStaff: build.query<GetTotalUsersStaffResponse, void>({
+      query: () => ({
+        url: `Dashboard/TotalUsersStaff`,
+        method: 'GET'
+      })
+    }),
+    getTotalItemsInStock: build.query<GetTotalItemsInStockResponse, void>({
+      query: () => ({
+        url: `Dashboard/TotalItemsInStock`,
+        method: 'GET'
+      })
+    }),
+    getTotalReturnedOrders: build.query<GetTotalReturnedOrdersResponse, void>({
+      query: () => ({
+        url: `Dashboard/TotalReturnedOrders`,
+        method: 'GET'
+      })
+    })
+  })
+});
+
+export const {
+  useGetTotalRevenueQuery,
+  useGetTotalUsersCustomerQuery,
+  useGetTotalUsersStaffQuery,
+  useGetTotalItemsInStockQuery,
+  useGetTotalReturnedOrdersQuery
+} = dashboardApi;
